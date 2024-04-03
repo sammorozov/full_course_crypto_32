4c4fd36fcc67c222f7793ef5f1a0f2aa6b703b5d
sam <an.sem2001@yandex.ru>
2024-04-03 13:36:20 +0300

    delete and upd

--- deploy.js
+++ deploy.js
@@ -2,3 +2,20 @@
 
 // synch [solidity]
 // asynch [js]
+
+const { error } = require("console");
+
+
+
+async function main(){
+    console.log('hi');
+
+    let age = 11;
+    console.log(age);
+
+}
+
+
+main().then(() => process.exit(0)).catch((error) => {
+    console.error(error)
+})
