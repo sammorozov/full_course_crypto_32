b3705018b17c6f3a6e362a97d090908d65714f79
sam <an.sem2001@yandex.ru>
2024-04-04 01:20:21 +0300

    add ganache key testnet

--- deploy.js
+++ deploy.js
@@ -1,17 +1,13 @@
-
-
-// synch [solidity]
-// asynch [js]
-
-const { error } = require("console");
-
-
+const ethers = require("ethers");
 
 async function main(){
     console.log('hi');
 
     let age = 11;
     console.log(age);
+
+    const provider = new ethers.providers.JsonRpcProvider("https://0.0.0.0:8545");
+    const wallet = new ethers.Wallet()
 
 }
 
---
+++ encryptKey.js
@@ -0,0 +1,14 @@
+const ethers = require("ethers")
+const fs = require(fs-extra);
+require(dotenv).config();
+
+async function main(){
+
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
